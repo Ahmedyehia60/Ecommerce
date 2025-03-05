a2adea013ab555ae88ee4fb3d378d447346bf106
ahmed <ahmedyehia2715@gmail.com>
2025-03-06 01:17:56 +0200

    create js for active navbar links

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
 
 if (bar) {
-  bar.addEventListener('click', () => {
-    nav.classList.add('active');
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
   });
 }
 
-if(close) {
-  close.addEventListener('click', () => {
-    nav.classList.remove('active');
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
   });
 }
+
+// active page link on click
+
+const links = document.querySelectorAll("#navbar a");
+
+function activeLinks() {
+  links.forEach((link) => {
+    link.classList.remove("active");
+    if (link.href === window.location.href) {
+      link.classList.add("active");
+    }
+  });
+
+  links.foreach((link) => {
+    link.addEventListener("click", function () {
+      activeLinks();
+    });
+  });
+}
+
+activeLinks();
+
+
